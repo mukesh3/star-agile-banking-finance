0be782162972b2bab8e734e90b808f59da26035f
mukesh3 <mukesh3258@gmail.com>
2024-12-08 12:12:42 +0100

    Update capstone_project_1.tf

--- terraform/capstone_project_1.tf
+++ terraform/capstone_project_1.tf
@@ -129,13 +129,12 @@ provisioner "remote-exec" {
 }
 
 output "vpc-id" {
-
-value = aws_vpc.sa-vpc.id
-
+  value = aws_vpc.sa-vpc.id
 }
-
 output "instance-id" {
-
-value = aws_instance.myec2.id
-
+  value = aws_instance.myec2.id
+}
+output "instance_public_ip" {
+  description = "Public IP address of the EC2 instance"
+  value       = aws_instance.myec2.public_ip
 }
